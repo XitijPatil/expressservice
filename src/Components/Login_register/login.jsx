642e07f201c060960c14781d2d11faef13ffbc30
Patil <kshitij_patil1@persistent.com>
2021-08-23 19:27:41 +0530

    Testing added

--- src/Components/Login_register/login.jsx
+++ src/Components/Login_register/login.jsx
@@ -70,7 +70,7 @@ export class Login extends React.Component {
     if (this.validateForm()) {
       this.postData();
         let fields = {};
-        fields["usernamez"] = "";
+        fields["username"] = "";
         fields["password"] = "";
         this.setState({fields:fields});
        
